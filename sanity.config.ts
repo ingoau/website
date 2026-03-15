58f07316462b224dcbc5732b5a3526d53e3e9997
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:29:19 +1100

    reanme

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
 export default defineConfig({
-  name: "website",
-  title: "Website",
+  name: "ingo.au",
+  title: "ingo.au",
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID!,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET!,
   plugins: [structureTool()],
