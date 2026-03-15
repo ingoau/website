9ac96666b919800c67a812d0a4134f283b091e08
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:27:22 +1100

    use import.meta.env

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,12 @@
 // sanity.config.ts
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
-import { loadEnv } from "vite";
-
-const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
-  process.env.NODE_ENV || "",
-  process.cwd(),
-  "",
-);
 
 export default defineConfig({
   name: "website",
   title: "Website",
-  projectId: PUBLIC_SANITY_PROJECT_ID!,
-  dataset: PUBLIC_SANITY_DATASET!,
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID!,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET!,
   plugins: [structureTool()],
   schema: {
     types: [
