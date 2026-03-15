47fa7d62ca28992db7fd1c704173a1dc2410423f
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:32:39 +1100

    add plugins

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,16 @@
 // sanity.config.ts
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { media } from "sanity-plugin-media";
+import { codeInput } from "@sanity/code-input";
 
 export default defineConfig({
   name: "ingo.au",
   title: "ingo.au",
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID!,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET!,
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool(), media(), codeInput()],
   schema: {
     types: [
       /* your content types here*/
