ed0975e4505014068f7fb1018e6fc29ac1e82057
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:25:19 +1100

    update to use loadenv

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,19 @@
 // sanity.config.ts
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { loadEnv } from "vite";
+
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV || "",
+  process.cwd(),
+  "",
+);
 
 export default defineConfig({
   name: "website",
   title: "Website",
-  projectId: process.env.PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.PUBLIC_SANITY_DATASET!,
+  projectId: PUBLIC_SANITY_PROJECT_ID!,
+  dataset: PUBLIC_SANITY_DATASET!,
   plugins: [structureTool()],
   schema: {
     types: [
