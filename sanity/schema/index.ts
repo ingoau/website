1b9370ad6f7177cd238097cc2b989ed1d7d94249
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:36:59 +1100

    add block content type

--- sanity/schema/index.ts
+++ sanity/schema/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { post } from "./post";
+import { blockContent } from "./blockContent";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post],
+  types: [blockContent, post],
 };
