b3b28f204046d3055df5011817716fb7bb67ef30
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:33:24 +1100

    add schema thingy

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { media } from "sanity-plugin-media";
 import { codeInput } from "@sanity/code-input";
+import { schema } from "./sanity/schema";
 
 export default defineConfig({
   name: "ingo.au",
@@ -11,9 +12,5 @@ export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID!,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET!,
   plugins: [structureTool(), visionTool(), media(), codeInput()],
-  schema: {
-    types: [
-      /* your content types here*/
-    ],
-  },
+  schema,
 });
