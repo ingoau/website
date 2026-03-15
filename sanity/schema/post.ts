280e85b1c22f657241e86d193a1fae1829d9db80
Ingo Wolf <me@ingowolf.au>
2026-03-15 23:36:06 +1100

    add post type

---
+++ sanity/schema/post.ts
@@ -0,0 +1,39 @@
+import { File } from "lucide-react";
+import { defineField, defineType } from "sanity";
+
+export const post = defineType({
+  name: "post",
+  title: "Post",
+  type: "document",
+  icon: File,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    // TODO: add tags
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
+    defineField({
+      name: "body",
+      type: "blockContent",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+    },
+    prepare(selection) {
+      return { ...selection };
+    },
+  },
+});
