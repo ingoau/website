93aa7b06b60f2ae7d84d2c09cd6548a18b65f76a
Ingo Wolf <me@ingowolf.au>
2026-03-20 00:01:26 +1100

    feat: load images

--- src/lib/sanity/queries.ts
+++ src/lib/sanity/queries.ts
@@ -1,6 +1,17 @@
+import {
+  createImageUrlBuilder,
+  type SanityImageSource,
+} from "@sanity/image-url";
 import { defineQuery } from "groq";
+import { sanityClient } from "sanity:client";
 
 export const postsQuery = defineQuery(`*[
   _type == "post"
   && defined(slug.current)
 ]|order(publishedAt desc){_id, title, slug, image, publishedAt}`);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
+
+const builder = createImageUrlBuilder(sanityClient);
