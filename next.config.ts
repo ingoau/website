53ef40b94a12e5f56f387151f83b3236ec102057
Ingo Wolf <me@ingo.au>
2026-05-29 23:09:24 +1000

    static export

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import { client } from "./sanity/lib/client";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: "export",
 };
 
 const withMDX = createMDX({
